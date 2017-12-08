e1218d80215c1a5c511b0a428878d238e5112665
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-12-08 20:51:53 +0700

    nambahin click exit

--- MenuUpgrade.h
+++ MenuUpgrade.h
@@ -10,17 +10,18 @@ public:
 	MenuUpgrade(ImageFrame *parent);
 	~MenuUpgrade();
 	void OnPaint(wxPaintEvent &event);
+	void OnClickExit(wxCommandEvent &event);
 
 	Hero *mirai;
 private:
 	ImageFrame *parentFrame;
 	wxString fileLocation;
 	wxBitmap *map, *buttonwindow, *upgrade, *chibi, *moneypanel;
-	wxBitmap *bitmapstatus, *bitmapbonds, *bitmapinvent, *bitmapskill;
+	wxBitmap *bitmapstatus, *bitmapbonds, *bitmapinvent, *bitmapskill, *bitmapexit;
 	wxBitmap *upattack, *uppotion, *upshield, *tupattack, *tuppotion, *tupshield;
 	wxBitmap *attackitem1, *attackitem2, *shielditem1, *shielditem2, *potionitem1, *potionitem2;
 	wxBitmap *money1, *money2, *money3;
-	wxBitmapButton *buttonstatus, *buttonbonds, *buttoninvent, *buttonskill;
+	wxBitmapButton *buttonstatus, *buttonbonds, *buttoninvent, *buttonskill, *buttonexit;
 
 	void LoadAllBitmap();
 	void LoadMapBitmap();
