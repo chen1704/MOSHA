6977ac242461aaba50757a7fe4c281b8cab2dd52
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-12-08 20:48:05 +0700

    Menu Inventory

--- MenuInvent.h
+++ MenuInvent.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "wx\wx.h"
 #include "ImageFrame.h"
+#include "Hero.h"
 
 class MenuInvent : public wxWindow
 {
@@ -8,16 +9,21 @@ public:
 	MenuInvent(ImageFrame *parent);
 	~MenuInvent();
 	void OnPaint(wxPaintEvent &event);
+	void OnClickExit(wxCommandEvent &event);
+
+	Hero *mirai;
 private:
 	ImageFrame *parentFrame;
 	wxString fileLocation;
-	wxBitmap *map, *buttonWindow;
-	wxBitmap *bitmapstatus, *bitmapbonds, *bitmapinvent, *bitmapskill;
+	wxBitmap *map, *buttonWindow, *inventory, *chibi;
+	wxBitmap *bitmapstatus, *bitmapbonds, *bitmapinvent, *bitmapskill, *bitmapexit;
+	wxBitmap *balok, *batu, *kayu, *bara, *tanah, *diamond;
+	wxBitmapButton *buttonstatus, *buttonbonds, *buttoninvent, *buttonskill, *buttonexit;
 
 	void LoadAllBitmap();
 	void LoadMapBitmap();
-	void LoadbuttonWindowBitmap();
-	void LoadUpgradeBitmap();
+	void LoadMenuBitmap();
+	void LoaditemBitmap();
 	DECLARE_EVENT_TABLE()
 };
 
