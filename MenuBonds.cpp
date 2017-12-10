4232e5b973a0c1d12f7aa3ace2dddb2e9d1c95fb
chen1704 <32873349+chen1704@users.noreply.github.com>
2017-12-10 17:06:29 +0700

    terhubung

--- MenuBonds.cpp
+++ MenuBonds.cpp
@@ -7,6 +7,13 @@
 BEGIN_EVENT_TABLE(MenuBonds, wxWindow)
 EVT_PAINT(MenuBonds::OnPaint)
 EVT_BUTTON(1001, MenuBonds::OnClickExit)
+EVT_BUTTON(1011, MenuBonds::OnClickShimura)
+EVT_BUTTON(1012, MenuBonds::OnClickHijikata)
+EVT_BUTTON(1013, MenuBonds::OnClickSakata)
+EVT_BUTTON(1007, MenuBonds::OnClickStatus)
+//EVT_BUTTON(1008, Map::ClickButtonBonds)
+EVT_BUTTON(1009, MenuBonds::OnClickInvent)
+EVT_BUTTON(1010, MenuBonds::OnClickUpgrade)
 END_EVENT_TABLE()
 
 MenuBonds::MenuBonds(ImageFrame *parent) : wxWindow(parent, wxID_ANY), parentFrame(parent) {
@@ -24,6 +31,11 @@ MenuBonds::MenuBonds(ImageFrame *parent) : wxWindow(parent, wxID_ANY), parentFra
 	buttoninvent = new wxBitmapButton(this, 1009, *bitmapinvent, wxPoint(138, 621), wxDefaultSize, wxBORDER_MASK);
 	buttonskill = new wxBitmapButton(this, 1010, *bitmapskill, wxPoint(32, 620), wxDefaultSize, wxBORDER_MASK);
 	buttonexit = new wxBitmapButton(this, 1001, *bitmapexit, wxPoint(397, 8), wxDefaultSize, wxBORDER_NONE);
+
+	rewardshimura = new wxBitmapButton(this, 1011,*bitmapshimura, wxPoint(339, 100), wxDefaultSize, wxBORDER_NONE);
+	rewardhijikata = new wxBitmapButton(this, 1012, *bitmaphijikata, wxPoint(88, 233), wxDefaultSize, wxBORDER_NONE);
+	rewardsakata = new wxBitmapButton(this, 1013, *bitmapsakata, wxPoint(338, 352
+	), wxDefaultSize, wxBORDER_NONE);
 
 }
 
@@ -44,10 +56,10 @@ void MenuBonds::OnPaint(wxPaintEvent &event) {
 	pdc.DrawBitmap(*description, wxPoint(142, 494), true);
 	pdc.DrawBitmap(*shimura, wxPoint(94, 71), true);
 	pdc.DrawBitmap(*hijikata, wxPoint(281, 175), true);
-	pdc.DrawBitmap(*sakata, wxPoint(101, 288), true);
+	pdc.DrawBitmap(*sakata, wxPoint(94, 288), true);
 	pdc.DrawBitmap(*nameshimura, wxPoint(210, 93), true);
 	pdc.DrawBitmap(*namehijikata, wxPoint(177, 221), true); //atau 171
-	pdc.DrawBitmap(*namesakata, wxPoint(234, 341), true);
+	pdc.DrawBitmap(*namesakata, wxPoint(215, 341), true);
 
 	//drawnullheart on three boys
 	//shimura null
@@ -59,19 +71,19 @@ void MenuBonds::OnPaint(wxPaintEvent &event) {
 	pdc.DrawBitmap(*heartnull, wxPoint(194, 249), true);
 	pdc.DrawBitmap(*heartnull, wxPoint(234, 249), true);
 	//sakata null
-	pdc.DrawBitmap(*heartnull, wxPoint(234, 368), true);
-	pdc.DrawBitmap(*heartnull, wxPoint(274, 368), true);
-	pdc.DrawBitmap(*heartnull, wxPoint(314, 368), true);
+	pdc.DrawBitmap(*heartnull, wxPoint(212, 368), true);
+	pdc.DrawBitmap(*heartnull, wxPoint(252, 368), true);
+	pdc.DrawBitmap(*heartnull, wxPoint(292, 368), true);
 
 	//bond +1
 	if (mirai->bondshi == 1) pdc.DrawBitmap(*hearthalf, wxPoint(212, 126));
 	if (mirai->bondhij == 1) pdc.DrawBitmap(*hearthalf, wxPoint(154, 249));
-	if (mirai->bondsak == 1) pdc.DrawBitmap(*hearthalf, wxPoint(234, 368));
+	if (mirai->bondsak == 1) pdc.DrawBitmap(*hearthalf, wxPoint(212, 368));
 
 	//bond + 2
 	if (mirai->bondshi == 2) pdc.DrawBitmap(*heartfull, wxPoint(212, 126));
 	if (mirai->bondhij == 2) pdc.DrawBitmap(*heartfull, wxPoint(154, 249));
-	if (mirai->bondsak == 2) pdc.DrawBitmap(*heartfull, wxPoint(234, 368));
+	if (mirai->bondsak == 2) pdc.DrawBitmap(*heartfull, wxPoint(252, 368));
 
 	//bond +3
 	if (mirai->bondshi == 3) {
@@ -83,8 +95,8 @@ void MenuBonds::OnPaint(wxPaintEvent &event) {
 		pdc.DrawBitmap(*hearthalf, wxPoint(194, 249));
 	}
 	if (mirai->bondsak == 3) {
-		pdc.DrawBitmap(*heartfull, wxPoint(234, 368));
-		pdc.DrawBitmap(*hearthalf, wxPoint(274, 368));
+		pdc.DrawBitmap(*heartfull, wxPoint(212, 368));
+		pdc.DrawBitmap(*hearthalf, wxPoint(252, 368));
 	}
 
 	//bond+4
@@ -97,8 +109,8 @@ void MenuBonds::OnPaint(wxPaintEvent &event) {
 		pdc.DrawBitmap(*heartfull, wxPoint(194, 249));
 	}
 	if (mirai->bondsak == 4) {
-		pdc.DrawBitmap(*heartfull, wxPoint(234, 368));
-		pdc.DrawBitmap(*heartfull, wxPoint(274, 368));
+		pdc.DrawBitmap(*heartfull, wxPoint(212, 368));
+		pdc.DrawBitmap(*heartfull, wxPoint(252, 368));
 	}
 
 	//bond+5
@@ -113,9 +125,9 @@ void MenuBonds::OnPaint(wxPaintEvent &event) {
 		pdc.DrawBitmap(*hearthalf, wxPoint(234, 249));
 	}
 	if (mirai->bondsak == 5) {
-		pdc.DrawBitmap(*heartfull, wxPoint(234, 368));
-		pdc.DrawBitmap(*heartfull, wxPoint(274, 368));
-		pdc.DrawBitmap(*hearthalf, wxPoint(314, 368));
+		pdc.DrawBitmap(*heartfull, wxPoint(212, 368));
+		pdc.DrawBitmap(*heartfull, wxPoint(252, 368));
+		pdc.DrawBitmap(*hearthalf, wxPoint(292, 368));
 	}
 
 	//bond +6
@@ -130,9 +142,10 @@ void MenuBonds::OnPaint(wxPaintEvent &event) {
 		pdc.DrawBitmap(*heartfull, wxPoint(234, 249));
 	}
 	if (mirai->bondsak == 6) {
-		pdc.DrawBitmap(*heartfull, wxPoint(234, 368));
-		pdc.DrawBitmap(*heartfull, wxPoint(274, 368));
-		pdc.DrawBitmap(*heartfull, wxPoint(314, 368));
+		pdc.DrawBitmap(*heartfull, wxPoint(212, 368));
+		pdc.DrawBitmap(*heartfull, wxPoint(252, 368));
+		pdc.DrawBitmap(*heartfull, wxPoint(292
+			, 368));
 	}
 
 }
@@ -143,10 +156,40 @@ void MenuBonds::OnClickExit(wxCommandEvent & event)
 	parentFrame->ShowMap();
 }
 
+void MenuBonds::OnClickShimura(wxCommandEvent &event) {
+	wxMessageOutputDebug().Printf("Get Reward from Shimura");
+}
+
+void MenuBonds::OnClickHijikata(wxCommandEvent &event) {
+	wxMessageOutputDebug().Printf("Get Reward from Hijikata");
+}
+
+void MenuBonds::OnClickSakata(wxCommandEvent &event) {
+	wxMessageOutputDebug().Printf("Get Reward from Sakata");
+}
+
+void MenuBonds::OnClickStatus(wxCommandEvent &event) {
+	wxMessageOutputDebug().Printf("click on status");
+	parentFrame->ShowMenuStatus();
+}
+
+void MenuBonds::OnClickInvent(wxCommandEvent &event) {
+	wxMessageOutputDebug().Printf("click on inventory");
+	parentFrame->ShowMenuInvent();
+}
+
+
+void MenuBonds::OnClickUpgrade(wxCommandEvent &event) {
+	wxMessageOutputDebug().Printf("click on upgrade skill");
+	parentFrame->ShowMenuUpgrade();
+}
+
+
 void MenuBonds::LoadAllBitmap() {
 	this->LoadMapBitmap();
 	this->LoadMenuBitmap();
 	this->LoadBondsBitmap();
+	this->LoadRewardBitmap();
 }
 
 void MenuBonds::LoadMapBitmap() {
@@ -234,4 +277,18 @@ void MenuBonds::LoadBondsBitmap() {
 	wxString hfull = wxFileName(fileLocation).GetPath() + wxT("\\bond heart bar full.png");
 	wxImage image9(hfull, wxBITMAP_TYPE_PNG);
 	heartfull = new wxBitmap(image9);
+}
+
+void MenuBonds::LoadRewardBitmap() {
+	wxString locshimura = wxFileName(fileLocation).GetPath() + wxT("\\get shimura.png");
+	wxImage image1(locshimura, wxBITMAP_TYPE_PNG);
+	bitmapshimura = new wxBitmap(image1);
+
+	wxString lochijikata = wxFileName(fileLocation).GetPath() + wxT("\\get hijikata.png");
+	wxImage image2(lochijikata, wxBITMAP_TYPE_PNG);
+	bitmaphijikata = new wxBitmap(image2);
+
+	wxString locsakata = wxFileName(fileLocation).GetPath() + wxT("\\get sakata.png");
+	wxImage image3(locsakata, wxBITMAP_TYPE_PNG);
+	bitmapsakata = new wxBitmap(image3);
 }
