68a6935e880eea0043845ad174f4ea8431170e23
Natasha Valentina <32843032+natval98@users.noreply.github.com>
2017-12-16 13:42:27 +0700

    push enemy

--- FINAL/Enemy.cpp
+++ FINAL/Enemy.cpp
@@ -1,5 +1,6 @@
 #include "Enemy.h"
-
+#include <wx/stdpaths.h>
+#include <wx/filename.h>
 
 
 Enemy::Enemy()
@@ -9,4 +10,82 @@ Enemy::Enemy()
 
 Enemy::~Enemy()
 {
+/*
+	for (int i = 0; i <= 16; i++) delete ma[i];
+	for (int i = 0; i <= 3; i++) delete mw[i];
+
+	for (int i = 1; i <= 4; i++) delete ww[i];
+	for (int i = 0; i <= 12; i++) delete wa[i];
+	for (int i = 1; i <= 8; i++) delete cw[i];
+	for (int i = 1; i <= 22; i++) delete ca[i];
+*/
 }
+
+/*
+void Enemy::LoadMageBitmap()
+{
+	wxStandardPaths &stdPaths = wxStandardPaths::Get();
+	fileLocation = stdPaths.GetExecutablePath();
+
+	for (int i = 0; i <= 17; i++) {
+		wxString elocatt;
+		elocatt = wxFileName(fileLocation).GetPath() + wxT("\\mageatt");
+		elocatt = elocatt + wxString::Format("%d", i);
+		elocatt = elocatt + wxT(".png");
+		wxImage emage(elocatt, wxBITMAP_TYPE_PNG);
+		ma[i] = new wxBitmap(emage);
+	}
+
+	for (int i = 0; i <= 3; i++) {
+		wxString elocatt;
+		elocatt = wxFileName(fileLocation).GetPath() + wxT("\\mageturn");
+		elocatt = elocatt + wxString::Format("%d", i);
+		elocatt = elocatt + wxT(".png");
+		wxImage emage(elocatt, wxBITMAP_TYPE_PNG);
+		mw[i] = new wxBitmap(emage);
+	}
+	
+}
+
+void Enemy::LoadWarriorBitmap()
+{
+	for (int i = 1; i <= 4; i++) {
+		wxString elocatt;
+		elocatt = wxFileName(fileLocation).GetPath() + wxT("\\warriorwalking");
+		elocatt = elocatt + wxString::Format("%d", i);
+		elocatt = elocatt + wxT(".png");
+		wxImage emage(elocatt, wxBITMAP_TYPE_PNG);
+		ww[i] = new wxBitmap(emage);
+	}
+
+	for (int i = 0; i <= 12; i++) {
+		wxString elocatt;
+		elocatt = wxFileName(fileLocation).GetPath() + wxT("\\warrioratas");
+		elocatt = elocatt + wxString::Format("%d", i);
+		elocatt = elocatt + wxT(".png");
+		wxImage emage(elocatt, wxBITMAP_TYPE_PNG);
+		wa[i] = new wxBitmap(emage);
+	}
+}
+
+void Enemy::LoadChitoseBitmap()
+{
+	for (int i = 1; i <= 8; i++) {
+		wxString elocatt;
+		elocatt = wxFileName(fileLocation).GetPath() + wxT("\\chitose walking");
+		elocatt = elocatt + wxString::Format("%d", i);
+		elocatt = elocatt + wxT(".png");
+		wxImage emage(elocatt, wxBITMAP_TYPE_PNG);
+		cw[i] = new wxBitmap(emage);
+	}
+
+	for (int i = 1; i <= 22; i++) {
+		wxString elocatt;
+		elocatt = wxFileName(fileLocation).GetPath() + wxT("\\chitose");
+		elocatt = elocatt + wxString::Format("%d", i);
+		elocatt = elocatt + wxT(".png");
+		wxImage emage(elocatt, wxBITMAP_TYPE_PNG);
+		ca[i] = new wxBitmap(emage);
+	}
+}
+*/
--- FINAL/Enemy.h
+++ FINAL/Enemy.h
@@ -1,8 +1,29 @@
 #pragma once
+#include "wx\wx.h"
+#include <string>
+using namespace std;
+
 class Enemy
 {
 public:
 	Enemy();
 	~Enemy();
+
+	int hp, hpmax, attack;
+	string name;
+
+
+//	wxBitmap *mw[5], *ma[20]; //battle1, battle4 [mage]
+/*
+	wxBitmap *ww[5], *wa[15]; //battle2, battle5 [warrior]
+	wxBitmap *cw[10], *ca[28]; //battle3, battle6 [chitose]
+*/
+	wxString fileLocation;
+
+//	void LoadMageBitmap();
+/*
+	void LoadWarriorBitmap();
+	void LoadChitoseBitmap();
+*/
 };
 
